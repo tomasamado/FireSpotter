e869c31c1357d91c5464979fe8b09dab02835d2f
Tom <tomasamadopeters@gmail.com>
2018-11-11 13:47:59 -0400

    Fetch Songs + Search bar

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+var SpotifyWebApi = require('react-native-spotify-web-api');
 import { createStackNavigator } from 'react-navigation';
 import { HomeScreen } from './app/screens/HomeScreen';
 import { SearchScreen } from './app/screens/SearchScreen';
 import { DetailsScreen } from './app/screens/DetailsScreen';
 
+var spotifyApi = new SpotifyWebApi({
+  clientId: '0c97682e61534996b733c2570805da2c',
+  clientSecret: 'ef3edeba7b174259b46609d44625863e',
+  redirectUri: 'http://localhost:8888/callback'
+});
+spotifyApi.setAccessToken('BQDX0BoUkVITrBd5UTQhcRDAcwClMT1xAV-HDrIh_Z_TyHVqTvFqaGReedef05tPPp2wlC6CrghMiQ_RFj_rFKBOIiACTt-mnuiqQO8QGuXthTRavxBLJyqypWrD2bQPruonWrQFPdBV56ZbqAG1eulGVw');
+
+
 
 export default class App extends React.Component {
+
   render() {
     return <RootStack />;
   }
