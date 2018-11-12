e72e6fdb4c091b914ae61e75b2cfcf69a95e71a9
Tom <tomasamadopeters@gmail.com>
2018-11-11 20:14:02 -0400

    Added bars

--- app/screens/DetailsScreen.js
+++ app/screens/DetailsScreen.js
@@ -1,21 +1,79 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, AppRegistry } from 'react-native';
 import { Card, Button } from 'react-native-elements'
+import AnimatedBar from "react-native-animated-bar";
+
+
+
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyApi = new SpotifyWebApi({
+    clientId: '0c97682e61534996b733c2570805da2c',
+    clientSecret: 'ef3edeba7b174259b46609d44625863e',
+    redirectUri: 'http://localhost:8888/callback'
+});
+spotifyApi.setAccessToken('BQCbKPC_rZoJVbprnFjSAOrqbgF5Xlt16gv0obhw5zQO8UKLWLQUhxtNLyk-9xe_PP2nm4Apq2oSyZ8GEk_YvWv1_lQEuo4M_CZm59yvRjRGaE4LB9eV0evAjMq6cCmDjr657LlQ-pji9cwuys67pQ6skw');
+
 
 
 export class DetailsScreen extends React.Component {
+
     static navigationOptions = {
         title: 'Details',
     };
 
+    state = {
+        progress: 0,
+        progress2:0,
+        progress3:0,
+        progress4:0,
+        progress5:0,
+    };
+
+    getFeatures(id) {
+        spotifyApi.getAudioFeaturesForTrack(id)
+            .then((features) => {
+                var audioFeatures = {
+                    danceability: features.body.danceability,
+                    energy: features.body.energy,
+                    acousticness: features.body.acousticness,
+                    instrumentalness: features.body.instrumentalness,
+                    valence: features.body.valence,
+                }
+
+                console.log(features.body.danceability);
+                console.log(features.body.energy);
+
+                const interval = setInterval(() => {
+                    if (this.state.progress > 0) return clearInterval(interval);
+                    this.setState(state => {
+                        return {
+                            progress: state.progress + audioFeatures.danceability,
+                            progress2: state.progress2 + audioFeatures.energy,
+                            progress3: state.progress2 + audioFeatures.acousticness,
+                            progress4: state.progress2 + audioFeatures.instrumentalness,
+                            progress5: state.progress2 + audioFeatures.valence
+                        };
+                    });
+                }, 1000);
+
+            }, (err) => {
+                console.log('Something went wrong!', err);
+            });
+    }
+
+    componentDidMount() {
+
+    }
+
     render() {
         const { navigation } = this.props;
         const item = navigation.getParam('item', {});
+        this.getFeatures(item.id);
 
         return (
             <View styles={styles.container}>
-                <Image 
-                    style={{ width: 280, height: 280, justifyContent: 'center', alignContent: 'center', alignItems: 'center', alignSelf: 'center', marginTop: 10}}
+                <Image
+                    style={{ width: 280, height: 280, justifyContent: 'center', alignContent: 'center', alignItems: 'center', alignSelf: 'center', marginTop: 10 }}
                     source={{ uri: item.cover }}
                 />
                 <Card title="">
@@ -31,6 +89,28 @@ export class DetailsScreen extends React.Component {
                         this.props.navigation.replace('Search');
                     }}
                 />
+                <AnimatedBar
+
+                    progress={this.state.progress}
+                    height={20}
+                    borderColor="#DDD"
+                    barColor="tomato"
+                    borderRadius={5}
+                    borderWidth={5}
+                    duration={2000}
+
+                />
+                <AnimatedBar
+
+                    progress={this.state.progress2}
+                    height={20}
+                    borderColor="#DDD"
+                    barColor="tomato"
+                    borderRadius={5}
+                    borderWidth={5}
+                    duration={2000}
+
+                />
             </View>
         );
     }
@@ -43,8 +123,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
-    image:{
-        width: 200, 
+    image: {
+        width: 200,
         height: 200,
         flex: 1,
         justifyContent: 'center',
--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, List, FlatList, Text } from 'react-native';
+import { View, Button, List, FlatList, Text, AppRegistry } from 'react-native';
 import { SearchBar } from 'react-native-elements'
 import { SongComponent } from '../components/SongComponent/SongComponent';
 import { StyleSheet } from 'react-native';
@@ -11,7 +11,7 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret: 'ef3edeba7b174259b46609d44625863e',
     redirectUri: 'http://localhost:8888/callback'
 });
-spotifyApi.setAccessToken('BQDy5xDCcD8OLLwzgbNdruMKXCcGWm3aJjPM4fjqUmD9gvTS7NvAEDB_RdKcZ3Znp8czbD7vPRpeNMNQLUKFtaRkv0Cj6hdfvm_i03E5fUe-dIoHQ0waNgB7JwvNeFHcpG3ZuM_Ex_x1-r_osDIHD6_gXA');
+spotifyApi.setAccessToken('BQCbKPC_rZoJVbprnFjSAOrqbgF5Xlt16gv0obhw5zQO8UKLWLQUhxtNLyk-9xe_PP2nm4Apq2oSyZ8GEk_YvWv1_lQEuo4M_CZm59yvRjRGaE4LB9eV0evAjMq6cCmDjr657LlQ-pji9cwuys67pQ6skw');
 
 
 export class SearchScreen extends React.Component {
@@ -37,7 +37,8 @@ export class SearchScreen extends React.Component {
                     artist: data.body.tracks.items[i].artists[0].name,
                     album: data.body.tracks.items[i].album.name,
                     year: data.body.tracks.items[i].album.release_date.substring(0, 4),
-                    cover: data.body.tracks.items[i].album.images[1].url
+                    cover: data.body.tracks.items[i].album.images[1].url,
+                    id: data.body.tracks.items[i].id
                 });
             }
             this.setState({
